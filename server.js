4acaeb248f62abeaaa9a09f8e647f9e9667acd96
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-19 15:41:33 -0400

    Add validation to register user route

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 
 connectDB();
-app.get("/", (req, res) => res.send("API Running."));
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
