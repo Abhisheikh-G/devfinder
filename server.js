9f159e5ccd7004b7dc0a667c93d20d36da4e7a89
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-19 15:28:18 -0400

    Add routes, define in server.js

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const connectDB = require("./config/db");
 
 connectDB();
 app.get("/", (req, res) => res.send("API Running."));
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
