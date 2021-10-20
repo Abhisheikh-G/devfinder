2eb3fc9d6e65ff91fb8dc4bbfb54a7dbe0770b83
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-20 10:12:37 -0400

    Add Create and Delete Comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -149,4 +149,71 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+// @route POST api/posts/comment/:id
+// @desc Comment on a post
+// @access Private
+router.post(
+  "/comment/:id",
+  [auth, check("text", "Text is required to make a post.").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+      await post.save();
+      res.status(200).json(post.comments);
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ msg: "Server Error." });
+    }
+  }
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete comment by post id and comment id
+// @access Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment)
+      return res
+        .status(404)
+        .json({ msg: "Sorry, we couldn't find that comment." });
+
+    if (comment.user.toString() !== req.user.id)
+      return res
+        .status(401)
+        .json({ msg: "You can only delete your own comments." });
+
+    const removeIndex = post.comments.map((comment) =>
+      comment.user.toString().indexOf(req.user.id)
+    );
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.status(200).json({ msg: "Comment removed." });
+  } catch (error) {
+    console.log(error.message);
+
+    if (error.kind === "ObjectId")
+      return res.status(404).json({ msg: "Comment not found." });
+
+    return res.status(500).json({ msg: "Server Error." });
+  }
+});
 module.exports = router;
