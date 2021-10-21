6f42df18ad7d52ff0f49587b7a7498e3c7c38ce6
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-21 10:14:52 -0400

    Imported Link and converted a tags, added Login and Register to Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import LandingPage from "./components/layout/LandingPage";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <LandingPage />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={LandingPage} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
