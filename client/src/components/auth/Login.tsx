179ff556d55c292aebf5c5b684a3f1e732ed2c95
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-21 15:01:39 -0400

    Add state to Login & Register pages

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { Fragment, ChangeEvent, useState, FormEvent } from "react";
+import { Link } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+interface LogInForm {
+  email: string;
+  password: string;
+}
+
+const defaultState: LogInForm = {
+  email: "",
+  password: "",
 };
 
-export default Login;
+const LogIn = () => {
+  const [formData, setFormData] = useState(defaultState);
+  const { email, password } = formData;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!password) {
+      console.log("Passwords do not much.");
+    } else {
+      try {
+        console.log("Success");
+      } catch (error: any) {
+        console.log(error.response);
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength={6}
+            value={password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Log In" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default LogIn;
--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,19 +1,72 @@
-import React, { Fragment, useState } from "react";
+import { Fragment, ChangeEvent, useState, FormEvent } from "react";
+import { Link } from "react-router-dom";
+
+interface RegisterForm {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const defaultState: RegisterForm = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Register = () => {
-  const [formData, setFormData] = useState();
+  const [formData, setFormData] = useState(defaultState);
+  const { name, email, password, confirmPassword } = formData;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      console.log("Passwords do not much.");
+    } else {
+      try {
+        console.log("Success");
+        // const body = JSON.stringify({ name, email, password });
+        // const res = await axios.post("/api/users", body, {
+        //   headers: { "Content-Type": "application/json" },
+        // });
+        // console.log(res.data);
+      } catch (error: any) {
+        console.log(error.response);
+      }
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+            value={name}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -25,20 +78,26 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength={6}
+            value={password}
+            onChange={handleChange}
+            required
           />
         </div>
         <div className="form-group">
           <input
             type="password"
             placeholder="Confirm Password"
-            name="password2"
+            name="confirmPassword"
             minLength={6}
+            value={confirmPassword}
+            onChange={handleChange}
+            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
