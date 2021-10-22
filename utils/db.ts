57f9f18cabfdcc7753280a49d5c827fb54402413
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-21 20:03:01 -0400

    Migrate utils to typescript

--- utils/db.ts
+++ utils/db.ts
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
     console.log("MongoDB connected..");
-  } catch (error) {
+  } catch (error: any) {
     console.log(error.message);
     //Fails process if database does not connect
     process.exit(1);
   }
 };
-
-module.exports = connectDB;
