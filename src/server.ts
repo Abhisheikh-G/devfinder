e4a2aa7e14b2f037133ab1a598a5d7795445601a
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-11-04 11:10:42 -0400

    Prepare for deployment

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import authRoute from "./routes/api/auth";
 import postsRoute from "./routes/api/posts";
 import profileRoute from "./routes/api/profile";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -22,4 +23,11 @@ app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/posts", postsRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
